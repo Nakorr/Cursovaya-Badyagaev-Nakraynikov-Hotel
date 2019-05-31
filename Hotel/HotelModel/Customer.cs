e9c4fba0a871fca2287886bf8102b1de85239c3e
Nakorr <Nakorr777@gmail.com>
2019-05-31 22:06:53 +0400

    Step-1

---
+++ Hotel/HotelModel/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelModel
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerFIO { get; set; }
+        public string CustomerLogin { get; set; }
+        public string CustomerPassword { get; set; }
+        public string CustomerMail { get; set; }
+
+    }
+}
